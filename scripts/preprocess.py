be36805e3384c631baf9f51ab2dd110c4b1b5273
mightymjolnir <mightymjolnir@qq.com>
2024-06-21 17:07:06 +0800

    🦄 refactor: update preprocess.py

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -1,45 +1,16 @@
 import re
 import shutil
 from pathlib import Path
-from typing import NamedTuple
 
 import yaml
-
-
-class HTML(NamedTuple):
-    title: str
-    url: str
-    path: Path
-
-    _title_pattern = re.compile("<title>(.*)</title>")
-    _url_pattern = re.compile("^ url: (.*) $", re.MULTILINE)  # SingleFile added
-
-    @classmethod
-    def from_file(cls, path: Path) -> "HTML":
-        text = open(path, encoding="utf-8").read()
-        title = "Untitled"
-        url = ""
-        if match := cls._title_pattern.search(text):
-            title = match.group(1)
-        if match := cls._url_pattern.search(text):
-            url = match.group(1)
-        return cls(title=title, url=url, path=path)
-
-    def markdown_link(self) -> str:
-        return f"[{self.title}]({self.url})" if self.url else self.title
-
-    @property
-    def truncated_title(self, max_len: int = 20) -> str:
-        if len(self.title) > max_len:
-            return self.title[:max_len] + "..."
-        return self.title
-
+from datatype import HTML
 
 work_dir = Path(__file__).parent.parent
 data = yaml.safe_load(open(work_dir / "mkdocs.yml"))
 
 docs_dir = work_dir / "docs"
 html_dir = docs_dir / "html"
+shutil.rmtree(html_dir)
 shutil.copytree(work_dir / "html", html_dir)
 # search html files
 htmls = [HTML.from_file(i) for i in html_dir.rglob("*.html")]
@@ -53,7 +24,7 @@ if htmls:
             f.write("\n\n")  # make sure there is a blank line
             f.writelines([f"- {i.markdown_link()}\n" for i in htmls])
 
-    web_notes.extend({i.truncated_title: i.path.relative_to(docs_dir).as_posix()} for i in htmls)
+    web_notes.extend({i.truncated_title: Path(html_dir, i.filename).relative_to(docs_dir).as_posix()} for i in htmls)
 
     # update mkdocs.yml
     data["nav"] = data.get("nav", [])
