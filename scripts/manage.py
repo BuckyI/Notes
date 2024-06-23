a67a5b30f371d949a2b68484a421ea3b6f1dce97
mightymjolnir <mightymjolnir@qq.com>
2024-06-23 13:14:22 +0800

    🎈 perf: improve notification logit

--- scripts/manage.py
+++ scripts/manage.py
@@ -39,12 +39,13 @@ info = st.empty()
 if uploaded_file is not None:
     uploaded_file.name = datetime.now().strftime("%Y%m%d%H%M%S") + ".html"
     html = HTML.from_text(uploaded_file.read().decode("utf-8"), uploaded_file.name)
-    if overwrite and (files["title"] == html.title).any():  # overwrite existed old file
-        old_filename: str = files[files["title"] == html.title]["filename"].values[0]
-        uploaded_file.name = old_filename
-        st.warning(f"overwrite `{old_filename}` since they have same title: `{html.title}`.")
+    if (files["title"] == html.title).any():
+        info.warning(f"File with title '`{html.title}`' already exists.")
+        if overwrite:  # overwrite existed old file
+            old_filename: str = files[files["title"] == html.title]["filename"].values[0]
+            uploaded_file.name = old_filename
+            info.warning(f"Overwrite '**`{old_filename}`**' since it has title '`{html.title}`'.")
 
-    info.info(f"saving to `{uploaded_file.name}`, confirm?")
     if st.button("confirm"):
         path = source / uploaded_file.name
         path.write_bytes(uploaded_file.getvalue())
